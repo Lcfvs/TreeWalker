d83a4e80885e7c2a2326f2862112afe61e1bc6db
Lcf.vs <michael.rouges@gmail.com>
2014-04-17 18:17:05 +0200

    tutos 0.0.3

---
+++ tutorials/File/018-file.acquire.js
@@ -0,0 +1,41 @@
+var init,
+    TreeWalker,
+    oninitdone,
+    onwalkerfile,
+    onfile,
+    onerror,
+    emitter;
+
+init = require('../init');
+TreeWalker = require('../../TreeWalker');
+
+oninitdone = function oninitdone(path) {
+    var treeWalker;
+
+    treeWalker = new TreeWalker(path, './directory/subDirectory/hello.txt');
+
+    treeWalker.once('file', onwalkerfile);
+    treeWalker.once('error', onerror);
+};
+
+onwalkerfile = function onwalkerfile(file) {
+    var handler;
+    
+    console.log('file.acquire()');
+    
+    handler = file.acquire();
+    
+    handler.once('file', onfile);
+    handler.once('error', onerror);
+};
+
+onfile = function onfile(file) {
+    console.log('file : ' + file);
+};
+
+onerror = console.log.bind(console, 'error');
+
+emitter = init();
+
+emitter.once('done', oninitdone);
+emitter.once('error', onerror);
